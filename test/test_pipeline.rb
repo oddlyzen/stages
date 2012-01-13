f409356ca8ff966cbe1592d97f7acfc59edc8733
mediocretes <naginata@gmail.com>
2012-01-13 16:13:15 -0500

    a little cleanup of pointless |x|

--- test/test_pipeline.rb
+++ test/test_pipeline.rb
@@ -10,19 +10,19 @@ class TestPipeline < MiniTest::Unit::TestCase
     mx3.source = evens 
     mx7.source = mx3
     
-    result = (0..2).map{ |x| mx7.run }
+    result = (0..2).map{ mx7.run }
     assert_equal([0, 42, 84], result)
   end
   
   test 'pipeline pipe syntax works' do
     pipeline = Evens.new | MultiplesOf.new(3) | MultiplesOf.new(7)
-    result = (0..2).map{ |x| pipeline.run }
+    result = (0..2).map{ pipeline.run }
     assert_equal([0, 42, 84], result)
   end
   
   test 'block stages work' do
     pipeline = Evens.new | Map.new{ |x| x * 3} | Select.new{ |x| x % 7 == 0}
-    result = (0..2).map{ |x| pipeline.run }
+    result = (0..2).map{ pipeline.run }
     assert_equal([0, 42, 84], result)
   end
   
--- test/test_stages.rb
+++ test/test_stages.rb
@@ -11,33 +11,34 @@ class TestStages < MiniTest::Unit::TestCase
     
   test 'select' do
     pipeline = Evens.new | Select.new{ |val| val > 6}
-    result = (0..2).map{ |x| pipeline.run }
+    result = (0..2).map{ pipeline.run }
     assert_equal([8, 10, 12], result)
   end
   
   test 'map' do
     pipeline = Evens.new | Map.new{ |val| val * 3}
-    result = (0..2).map{ |x| pipeline.run }
+    result = (0..2).map{ pipeline.run }
     assert_equal([0, 6, 12], result)
   end
   
   test 'multiples_of' do
     pipeline = Evens.new | MultiplesOf.new(3)
-    result = (0..3).map{ |x| pipeline.run }
+    result = (0..3).map{ pipeline.run }
     assert_equal([0, 6, 12, 18], result)
   end
   
   test 'each_element' do
     pipeline = EachElement.new([1, 2, 3])
-    result = (0..2).map{ |x| pipeline.run }
+    result = (0..2).map{ pipeline.run }
     assert_equal([1, 2, 3], result)
   end
   
   test 'hash_lookup' do
     pipeline = EachElement.new([:do, :re, :mi]) | HashLookup.new(sing)
-    result = (0..2).map{ |x| pipeline.run }
+    result = (0..2).map { pipeline.run }
     assert_equal(['doe a deer a female deer', 'ray a drop of golden sun', 'me a name I call myself'], result)
   end   
+
   
   def sing
     { :do => 'doe a deer a female deer',
